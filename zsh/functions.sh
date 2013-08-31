e3812f401e5260b470634a48bc5ce2ef2aea9357
Tim Jarratt <tjarratt@gmail.com>
2013-08-31 13:35:17 -0400

    Add some random facts to my git output

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -1,3 +1,4 @@
+# git helpers
 function git-stash-recent-commits($n) {
   for ($i = 0; $i < $n, $i++) {
     export git_last_stash_name = git log --format=%B -n 1 HEAD | head -1 | sed -e 's/ /_/g';
@@ -5,11 +6,14 @@ function git-stash-recent-commits($n) {
     git stash save $git_last_stash_name
   }
 }
-
 function gitdiff {
-  git diff | vim -R -;
+  fact; git diff | vim -R -;
+}
+function rebasei {
+  fact; git rebase --interactive "$@";
 }
 
-function rebasei {
-  git rebase --interactive "$@";
-}
+# random facts
+alias fact="/usr/local/Cellar/elinks/0.11.7/bin/elinks -dump randomfunfacts.com | sed -n '/^| /p' | tr -d \|"
+alias fact="/usr/local/Cellar/elinks/0.11.7/bin/elinks -dump randomfunfacts.com | sed -n '/^| /p' | tr -d \|"
+PROMPT_COMMAND='git status 2>&1 | grep "On branch" | grep -v master'
