4bc35e98140cfa55ed2ba6267c9ad9af97eeee01
Tim Jarratt <tjarratt@gmail.com>
2013-09-01 12:51:39 -0400

    Fix syntax for bash-style for loop

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -1,10 +1,11 @@
 # git helpers
-function git-stash-recent-commits($n) {
-  for ($i = 0; $i < $n, $i++) {
+function git-stash-recent-commits {
+  for i in {0..$1}
+  do
     export git_last_stash_name = git log --format=%B -n 1 HEAD | head -1 | sed -e 's/ /_/g';
     git revert HEAD~1
     git stash save $git_last_stash_name
-  }
+  done
 }
 function gitdiff {
   fact; git diff | vim -R -;
