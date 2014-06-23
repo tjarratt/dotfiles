26a0e0d3272e812df9a4eded5aa5438aa878bc93
Tim Jarratt <tjarratt@gmail.com>
2014-06-22 22:13:44 -0700

    Add a function to run go tests

--- emacs.d/tj-go.el
+++ emacs.d/tj-go.el
@@ -4,4 +4,9 @@
 (add-to-list 'load-path "/home/you/goroot/misc/emacs/")
 (add-hook 'before-save-hook 'gofmt-before-save)
 
+(defun go-test ()
+  "Runs tests for package in current buffer"
+  (interactive)
+  (compile (concat "ginkgo " (file-name-directory (buffer-file-name)))))
+
 (provide 'tj-go)
