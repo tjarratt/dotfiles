453fb3f371f45708a89a7617eccd5641dbd2e6f0
Anand Gaitonde and Tim Jarratt <agaitonde@pivotallabs.com>
2014-06-26 13:43:49 -0700

    Toggle between test and implementation for go-mode

--- emacs.d/tj-go.el
+++ emacs.d/tj-go.el
@@ -4,11 +4,20 @@
 (add-to-list 'load-path "/home/you/goroot/misc/emacs/")
 (add-hook 'before-save-hook 'gofmt-before-save)
 
+;; running tests
 (defun go-test ()
   "Runs tests for package in current buffer"
   (interactive)
   (compile (concat "ginkgo " (file-name-directory (buffer-file-name)))))
 
+
+;; test toggling
+(eval-after-load 'find-file
+  '(progn
+     (add-to-list cc-other-file-alist '(("_test.go"   (".go"))))
+     (add-to-list cc-other-file-alist '((".go"   ("_test.go"))))))
+
 (define-key tj-map (kbd "r t") 'go-test)
+(global-set-key (kbd "M-T") 'ff-find-other-file)
 
 (provide 'tj-go)
